0581053e34ac5ed24b85dfc1151b36c754db0bc9
Carl ERIKSSON <carl.eriksson@soprasteria.com>
2018-10-26 19:19:34 +0200

    Skeleton monitor

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace CPUMon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cpuLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.memoryLabel = new System.Windows.Forms.Label();
+            this.sysLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cpuLabel
+            // 
+            this.cpuLabel.AutoSize = true;
+            this.cpuLabel.BackColor = System.Drawing.Color.Transparent;
+            this.cpuLabel.Font = new System.Drawing.Font("Poor Richard", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cpuLabel.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.cpuLabel.Location = new System.Drawing.Point(36, 19);
+            this.cpuLabel.Name = "cpuLabel";
+            this.cpuLabel.Size = new System.Drawing.Size(48, 22);
+            this.cpuLabel.TabIndex = 0;
+            this.cpuLabel.Text = "CPU";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 0;
+            // 
+            // memoryLabel
+            // 
+            this.memoryLabel.AutoSize = true;
+            this.memoryLabel.BackColor = System.Drawing.Color.Transparent;
+            this.memoryLabel.Font = new System.Drawing.Font("Poor Richard", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.memoryLabel.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.memoryLabel.Location = new System.Drawing.Point(37, 62);
+            this.memoryLabel.Name = "memoryLabel";
+            this.memoryLabel.Size = new System.Drawing.Size(75, 22);
+            this.memoryLabel.TabIndex = 2;
+            this.memoryLabel.Text = "Memory";
+            // 
+            // sysLabel
+            // 
+            this.sysLabel.AutoSize = true;
+            this.sysLabel.Font = new System.Drawing.Font("Poor Richard", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sysLabel.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.sysLabel.Location = new System.Drawing.Point(38, 113);
+            this.sysLabel.Name = "sysLabel";
+            this.sysLabel.Size = new System.Drawing.Size(58, 19);
+            this.sysLabel.TabIndex = 3;
+            this.sysLabel.Text = "System";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.sysLabel);
+            this.Controls.Add(this.memoryLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cpuLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Mon";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label cpuLabel;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label memoryLabel;
+        private System.Windows.Forms.Label sysLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace CPUMon
+{
+    public partial class Form1 : Form
+    {
+
+        private PerformanceCounter cpuCounter = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
+        private PerformanceCounter memCounter = new PerformanceCounter("Memory", "Available MBytes");
+
+        private PerformanceCounter sysCounter = new PerformanceCounter("System", "System Up Time");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick_1(object sender, System.EventArgs e)
+        {
+            TimeSpan t = TimeSpan.FromSeconds((int)sysCounter.NextValue());
+
+            string answer = string.Format("{0:D2}h:{1:D2}m:{2:D2}s:{3:D3}ms",
+                            t.Hours,
+                            t.Minutes,
+                            t.Seconds,
+                            t.Milliseconds);
+
+            cpuLabel.Text = $"CPU: {(int)cpuCounter.NextValue()} %";
+            memoryLabel.Text = $"Available Memory: {(int)memCounter.NextValue()} MB";
+            sysLabel.Text = $"System up for: {t}";
+        }
+
+    }
+}
